ca742abf59eef6e1e1749d5c19cd93e4b35fb1ac
Magnus Deininger <magnus@ef.gy>
2016-03-06 14:19:55 -0500

    this ought to get the ball rolling as far as a debian/raspbian package goes

---
+++ debian/init.d
@@ -0,0 +1,19 @@
+#!/bin/sh
+# kFreeBSD do not accept scripts as interpreters, using #!/bin/sh and sourcing.
+if [ true != "$INIT_D_SCRIPT_SOURCED" ] ; then
+    set "$0" "$@"; INIT_D_SCRIPT_SOURCED=true . /lib/init/init-d-script
+fi
+### BEGIN INIT INFO
+# Provides:          rpi-ups-pico
+# Required-Start:    $syslog
+# Required-Stop:     $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: PIco UPS control daemon.
+# Description:       PIco UPS control daemon.
+### END INIT INFO
+
+# Author: Magnus Deininger <magnus@ef.gy>
+
+DESC="rpi-ups-pico"
+DAEMON=/sbin/picod
